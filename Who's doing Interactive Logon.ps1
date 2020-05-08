507586fd26acb8dd9ee1f5c0e9c272748f955908
Mark Silvey <39961687+silveyy@users.noreply.github.com>
2020-05-08 16:40:26 -0700

    Automated commit

---
+++ Who's doing Interactive Logon.ps1
@@ -0,0 +1,7 @@
+﻿$ComputerLocalHost = Get-Content env:computername
+$UserProperty = @{n="User";e={(New-Object System.Security.Principal.SecurityIdentifier $_.ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])}}
+$TypeProperty = @{n="Action";e={if($_.EventID -eq 7001) {"Logon"} else {"Logoff"}}}
+$TimeProperty = @{n="Time";e={$_.TimeGenerated}}
+$MachineNameProperty = @{n="MachineName";e={$_.MachineName}}
+
+Get-EventLog System -Source Microsoft-Windows-Winlogon -ComputerName $ComputerLocalHost | select $UserProperty, $TypeProperty,$TimeProperty,$MachineNameProperty 
