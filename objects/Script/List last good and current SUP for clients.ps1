1b5a8a06def177dcc6be2af19b09e64be7e21bad
Meghan Stewart <33289333+mestew@users.noreply.github.com>
2020-10-02 12:40:58 -0700

    Automated commit

---
+++ objects/Script/List last good and current SUP for clients.ps1
@@ -0,0 +1,4 @@
+﻿$Last = (Get-CIMInstance -Namespace "root\CCM\Scanagent" -Class "CCM_SUPLocationList").LastSuccessScanPath
+$Current= Write-Output (Get-CIMInstance -Namespace "root\CCM\Scanagent" -Class "CCM_SUPLocationList").CurrentScanPath
+Write-Host "LastGoodSUP- $last"
+Write-Host "CurrentSUP- $current"
